4ed1d409556a35b3af4a8d0524a959b4a4e8320c
Magda <m.lado@wp.pl>
2023-04-15 21:20:36 +0200

    new datbase

--- server.py
+++ server.py
@@ -19,21 +19,18 @@ class KPMP(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     pack_id = db.Column(db.Text, nullable=False)
     receive_time = db.Column(db.DateTime, nullable=False)
+    mag_diff = db.Column(db.Float, nullable=True)
     temp = db.Column(db.Float, nullable=True)
     hum = db.Column(db.Float, nullable=True)
-    acc_x = db.Column(db.Float, nullable=True)
-    acc_y = db.Column(db.Float, nullable=True)
-    acc_z = db.Column(db.Float, nullable=True)
-
+    courier_id = db.Column(db.Text, nullable=False)
     def to_dict(self):
         return {
-            "pack_id": self.pack_id,
             "time": self.receive_time,
-            "temperature": self.temp,
-            "humidility": self.hum,
-            "acc_x": self.acc_x,
-            "acc_y": self.acc_y,
-            "acc_z": self.acc_z
+            "pack_id": self.pack_id,
+            "mag_diff": self.mag_diff,
+            "temp": self.temp,
+            "hum": self.hum,
+            "courier_id": self.courier_id
         }
 
 with app.app_context():
@@ -56,127 +53,34 @@ def home():
                            headers=df.columns.values,
                            last_values=last_values)
 
-@app.route("/last-24hours")
-def plot_last_24hours():
-    try:
-        yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
-        con = sqlite3.connect("instance/KPMP.db")
-        df = pd.read_sql_query(f"SELECT * from KPMP WHERE receive_time >= '{yesterday}'", con)
-        con.close()
-    except Exception as e:
-        return f"Error! {e}"
-    else:
-        x_data = df[df.columns[1]].values.tolist()
-        y_data = [df[df.columns[i]].values.tolist() for i in range(2, 9)]
-        return render_template("data_plot_multiple.html",
-                               date_year=datetime.date.today().year,
-                               x_data=x_data,
-                               y_data=y_data,
-                               legend=[sensor.replace("_", " ") for sensor in df.columns[2:9].values.tolist()],
-                               title="last 24 hours temperature data from all sensors",
-                               x_label="time",
-                               y_label="Temperature/Humidity")
-
-@app.route("/temp-24hours")
-def plot_temp_24hours():
-    try:
-        yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
-        con = sqlite3.connect("instance/KPMP.db")
-        df = pd.read_sql_query(f"SELECT receive_time, temp, hum, acc_x, acc_y, acc_z from KPMP WHERE receive_time >= '{yesterday}'", con)
-        
-        con.close()
-    except Exception as e:
-        return f"Error! {e}"
-    else:
-        print(df.columns)
-        x_data = df[df.columns[0]].values.tolist()
-        y_data = [df[df.columns[i]].values.tolist() for i in range(1, 5)]
-        return render_template("data_plot_multiple.html",
-                               date_year=datetime.date.today().year,
-                               x_data=x_data,
-                               y_data=y_data,
-                               legend=[sensor.replace("_", " ") for sensor in df.columns[1:5].values.tolist()],
-                               title="last 24 hours data from all sensors",
-                               x_label="time",
-                               y_label="Temperature [\u2103]")
-
-
-@app.route("/all", methods=["GET"])
-def get_all_data():
-    try:
-        KPMP_datatable = db.session.query(KPMP).all()
-    except Exception as e:
-        print(e)
-        return jsonify(response={"error": f"Reading KPMP data failed. Error description {e}"}), 404
-    return jsonify(weathers=[data.to_dict() for data in KPMP_datatable])
-
-@app.route("/last", methods=["GET"])
-def get_last_data():
-    try:
-        KPMP_datatable = db.session.query(KPMP).order_by(KPMP.id.desc()).first()
-    except Exception as e:
-        print(e)
-        return jsonify(response={"error": f"Reading KPMP data failed. Error description {e}"}), 404
-    if KPMP_datatable is not None:
-        return jsonify(weathers=KPMP.to_dict())
-    else:
-        return jsonify(responseget_last_data={"error": "No entries in database"}), 400
 
 @app.route("/plot-data", methods=["GET"])
 def plot_data():
     con = sqlite3.connect("instance/KPMP.db")
     df = pd.read_sql_query("SELECT * from KPMP ", con)
 
+    print(df)
     con.close()
 
-    x_data = df[df.columns[1]].values.tolist()
-    y_data = df[df.columns[5]].values.tolist()
+    x_data = df[df.columns[2]].values.tolist()
+    y_data_temp = df[df.columns[4]].values.tolist()
+    y_data_hum = df[df.columns[5]].values.tolist()
+    y_data_magn = [df[df.columns[i]].values.tolist() for i in range(3, 5)]
+
 
     return render_template("data_plot.html",
                            date_year=datetime.date.today().year,
                            x_data=x_data,
-                           y_data=y_data,
+                           y_data_magn=y_data_magn,
+                           legend_magn=["magnituda", "magnituda"],
+                           y_data_temp=y_data_temp, 
+                           legend_temp="temeratura",
+                           y_data_hum=y_data_hum,
+                           legend_hum="wilgotność",
                            x_label=df.columns[1],
-                           y_label=df.columns[5])
-
-@app.route("/plot-all")
-def plot_all():
-    try:
-        con = sqlite3.connect("instance/KPMP.db")
-        df = pd.read_sql_query("SELECT * from KPMP ", con)
-        con.close()
-    except Exception as e:
-        return f"Error! {e}"
-    else:
-        x_data = df[df.columns[1]].values.tolist()
-        y_data = [df[df.columns[i]].values.tolist() for i in range(2, 9)]
-        return render_template("data_plot_multiple.html",
-                               date_year=datetime.date.today().year,
-                               x_data=x_data,
-                               y_data=y_data,
-                               legend=[sensor.replace("_", " ") for sensor in df.columns[2:9].values.tolist()],
-                               title="Data from all sensors",
-                               x_label="time",
-                               y_label="Temperature/Humidity")
-
-@app.route("/plot/<sensor_name>")
-def plot_one(sensor_name):
-    try:
-        con = sqlite3.connect("instance/KPMP.db")
-        df = pd.read_sql_query(f"SELECT receive_time, {sensor_name} from KPMP ", con)
-        con.close()
-    except Exception as e:
-        return f"Error! {e}"
-    else:
-        x_data = df["receive_time"].values.tolist()
-        y_data = df[sensor_name].values.tolist()
-
-        return render_template("data_plot.html",
-                               date_year=datetime.date.today().year,
-                               x_data=x_data,
-                               y_data=y_data,
-                               x_label="time",
-                               y_label="temperature [\u00B0C]")
+                           y_label_magn="[j.u.]",
+                           y_label_temp="[*C]",
+                           y_label_hum="[%]")
 
 # create record
 @app.route('/KPMP-data', methods=["POST"])
@@ -186,13 +90,12 @@ def KPMP_data():
 
         try:
             new_data = KPMP(receive_time=datetime.datetime.today(),
-                               pack_id=data["pack_id"],
-                               temp=data["temperature"],
-                               hum=data["humidility"],
-                               acc_x=data["acc_x"],
-                               acc_y=data["acc_y"],
-                               acc_z=data["acc_z"]
-                               )
+                            pack_id=data["pack_id"],
+                            mag_diff = data["mag_diff"],
+                            temp = data["temp"],
+                            hum = data["hum"],
+                            courier_id = data["courier_id"]
+                            )
 
             db.session.add(new_data)
             db.session.commit()
